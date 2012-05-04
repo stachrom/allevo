afb13c69f6731ce8171d4bdd5b73885e4d0eba5d
Roman Stachura <roman@stachura.ch>
2012-05-04 07:46:39 +0200

    remove password form fusion table

--- htdocs/js/yui3-gallery/gallery-weather.js
+++ htdocs/js/yui3-gallery/gallery-weather.js
@@ -21,7 +21,7 @@
         value: {
             compass: {N:"Norden", NO:"Nordosten", O:"Osten", SO:"Südosten",  S:"Süden", SW:"Südwesten", W:"Westen", NW:"Nordwesten"},
             wind: {chill:"gefühlt", speed:"Wind:", direction:"aus Richtung"},
-            atmosphere: {visibility:"Sichtweite:", humidity:"Luftfeuchtigkeit:", pressure:"Luftdruck:", rise:"steigend", drop:"fallend" },
+            atmosphere: {visibility:"Sichtweite:", humidity:"Luftfeuchtigkeit:", pressure:"Luftdruck:", rise:"steigend", drop:"fallend", steady:"konstant" },
             sun: {set:"Sonnenuntergang:", rise:"Sonnenaufgang:", unit:"Uhr"},
             aktualisierung : "Letzte Aktualisierung:"
         }
@@ -34,6 +34,7 @@
    Y.extend(LocalWeather, Y.Widget, {
  
       initializer : function() {
+         result = this_yqlRequest();
       },
  
       destructor : function() {
@@ -41,7 +42,7 @@
  
       //dom manipulatoin
       renderUI : function() {
-         this._addWeather();
+         this._addWeather(this.result);
       },
  
       //handle events
@@ -69,6 +70,7 @@
                
          return compass;
       },
+      
       /*
        * Pressure is heading up down or not even at all. 
       */
@@ -84,12 +86,13 @@
 
          return pressureDirection;
       },
+
       /**
-       * takes a date string and get out the Minutes of the Day.
-       * @method _timeOfTheDayToMinutes
-       * @param {date} Valide JS Date string --> new Date();
-       * 
-       * @private
+       * transform a Date into Minutes of the Day.
+       * @method _stringToValideDate
+       * @param {string} timeOfTheDay 8:28 pm / 8:38 am / 8:38:10
+       * modified.
+       * @privat
       */
       _timeOfTheDayToMinutes : function(date) {
 
@@ -105,65 +108,86 @@
        * transform the time of the day into date string.
        * @method _stringToValideDate
        * @param {string} timeOfTheDay 8:28 pm / 8:38 am / 8:38:10
-       * 
-       * @private
+       * modified.
+       * @privat
       */
       _timeToValideDate : function( timeOfTheDay ) {
-      
+
         var MDY = Y.DataType.Date.format(new Date(), {format:"%m/%d/%Y"});
         var today = Y.DataType.Date.parse(MDY+" "+timeOfTheDay);
-        
+
         return today;
       },
-
       //renders the current weather from YQL
-      _addWeather : function() {
-         var boundingBox = this.get("boundingBox"),
-             contentBox  = this.get("contentBox"),
-             location    = this.get('location'),
-             u           = this.get('u'),
-             layout      = this.get('layout'),
-             strings     = this.get("strings"),
-             that        = this;
-
+      _yqlRequest : function() {
+      
+         var location = this.get('location'),
+             u        = this.get('u');
+             
          Y.YQL( 'use "http://www.datatables.org/weather/weather.bylocation.xml" as we; select * from we where location="'+location+'" and unit="'+u+'"', function(r) { 
          
-            var result     = r.query.results.weather.rss.channel;
-                astronomy  = result.astronomy,
-                units      = result.units,
-                wind       = result.wind,
-                atmosphere = result.atmosphere,
-                lastupdate = result.item.condition.date;
-               
-            var sunrise     = that._timeToValideDate(astronomy.sunrise),
-                sunset      = that._timeToValideDate(astronomy.sunset),
-                sunrise_min = that._timeOfTheDayToMinutes(sunrise),
-                sunset_min  = that._timeOfTheDayToMinutes(sunset),
-                now_min     = that._timeOfTheDayToMinutes(new Date()),
-                direction   = wind.direction,
-                barometer   = that._pressureMove(atmosphere.rising, strings.atmosphere),
-                compass     = '';
-
-            var a = lastupdate.split(' ', 6);
-            var aktualiserung = Y.DataType.Date.parse(a[2]+' '+a[1]+', '+a[3]+' '+a[4] +' '+a[5]);
-                aktualiserung = Y.DataType.Date.format(aktualiserung, {format:"%H:%M"});
-
-            // day or night
-            if( sunrise_min <= now_min && sunset_min >= now_min ){
-               var meridium = 'd', class_m = '';
-            }else{
-               var meridium = 'n', class_m = 'night';
-            }
-               
-            var icon = 'assets/weather/'+result.item.condition.code + meridium  +'.png';
-            var background = 'assets/weather/background/'+result.item.condition.code + meridium  +'-106755.jpg';
+            var result = r.query.results.weather.rss.channel;
             
-            compass = that._windDirection(direction, strings.compass);
+            return result;
+         })
+      
+      }
+      
+      
 
-           var html = '<div id="yw-forecast" class="'+ class_m +'" style=" background:url(\''+background+'\'); background-size: cover;" >';
-               html += '<div id="yw-cond" class="'+ class_m +'" >'+result.location.city+'</div>';
+      //renders the current weather from YQL
+      _addWeather : function(result) {
+         var boundingBox = this.get("boundingBox"),
+             contentBox  = this.get("contentBox"),
+             layout      = this.get('layout'),
+             strings     = this.get("strings");
 
-            if(layout === "full"){
+         if ( result.title === "Yahoo! Weather - Error" ){
+            
+               var city = "City not found";
+               
+         }else{
+            
+               var astronomy  = result.astronomy,
+                   units      = result.units,
+                   wind       = result.wind,
+                   atmosphere = result.atmosphere,
+                   lastupdate = result.item.condition.date,
+                   city       = result.location.city,
+                   temp       = result.item.condition.temp,
+                   low        = result.item.forecast[0].low,
+                   high       = result.item.forecast[0].high;
+                  
+               var sunrise     = this._timeToValideDate(astronomy.sunrise),
+                   sunset      = this._timeToValideDate(astronomy.sunset),
+                   sunrise_min = this._timeOfTheDayToMinutes(sunrise),
+                   sunset_min  = this._timeOfTheDayToMinutes(sunset),
+                   now_min     = this._timeOfTheDayToMinutes(new Date()),
+                   direction   = wind.direction,
+                   barometer   = this._pressureMove(atmosphere.rising, strings.atmosphere),
+                   compass     = '';
+
+               var a = lastupdate.split(' ', 6);
+               var aktualiserung = Y.DataType.Date.parse(a[2]+' '+a[1]+', '+a[3]+' '+a[4] +' '+a[5]),
+                   aktualiserung = Y.DataType.Date.format(aktualiserung, {format:"%H:%M"});
+
+               // day or night
+               if( sunrise_min <= now_min && sunset_min >= now_min ){
+                  var meridium = 'd', class_m = '';
+               }else{
+                  var meridium = 'n', class_m = 'night';
+               }
+                  
+               var icon = 'assets/weather/'+result.item.condition.code + meridium  +'.png';
+               var background = 'assets/weather/background/'+result.item.condition.code + meridium  +'-106755.jpg';
+               
+               compass = this._windDirection(direction, strings.compass);
+         }
+         
+           var html  = '<div id="yw-forecast" class="'+ class_m +'" style=" background:url(\''+background+'\'); background-size: cover;" >';
+               html += '<div id="yw-cond" class="'+ class_m +'" >'+city+'</div>';
+
+            if(layout === "full" && result.title !== "Yahoo! Weather - Error" ){
                html += '<dl>'; 
                html += '<dt>'+strings.wind.chill+'</dt><dd>'+wind.chill+'°'+units.temperature+'</dd>';
                html += '<dt>'+strings.atmosphere.pressure+'</dt><dd>'+atmosphere.pressure+' '+ units.pressure+' '+ barometer +'</dd>'; 
@@ -174,18 +198,19 @@
                html += '<dt>'+strings.sun.set+'</dt><dd>'+Y.DataType.Date.format( sunset, {format:"%R"})+' '+strings.sun.unit+'</dd>';                 
                html += '</dl>'; 
             }               
-              
-               html += '<em>'+strings.aktualisierung+' '+aktualiserung+'</em>'; 
-               
+
+            if( result.title !== "Yahoo! Weather - Error" ){
+               html += '<em>'+strings.aktualisierung+' '+aktualiserung+' '+strings.sun.unit+'</em>'; 
                html += '<div class="forecast-temp">'+
-                          '<div id="yw-temp">'+result.item.condition.temp+'°</div>'+
-                           '<p>H:'+result.item.forecast[0].high+'° L: '+result.item.forecast[0].low+'°</p>'+
+                          '<div id="yw-temp">'+temp+'°</div>'+
+                           '<p>H:'+high+'° L: '+low+'°</p>'+
                           '</div>';
-               html += '<div class="forecast-icon" style=" background:url(\''+icon+'\'); _background-image/* */: none; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\''+icon+'\', sizingMethod="crop"); "></div>';                                         
+               html += '<div class="forecast-icon" style=" background:url(\''+icon+'\'); _background-image/* */: none; filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src=\''+icon+'\', sizingMethod="crop"); "></div>'; 
+            }               
                html += '</div>';  
 
             contentBox.setContent(html);
-         });
+     
       }
       
       
